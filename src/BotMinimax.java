a1a7236da318f92747dfb88eb78fe9283bf7b2d8
henryanandsr <13521004@std.stei.itb.ac.id>
2023-10-18 14:57:18 +0700

    fix : hill Climb and Minimax

--- src/BotMinimax.java
+++ src/BotMinimax.java
@@ -1,69 +1,96 @@
 import javafx.scene.control.Button;
 import javafx.util.Pair;
 import java.util.HashSet;
-
+import java.util.Random;
+import java.util.concurrent.*;
 public class BotMinimax extends Bot{
+    private int playerNumber;
+    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
+    private final long TIMEOUT = 5000;
+    private Pair<Integer, Integer>[] pairs;
     /***
      * Move with minimax alpha beta pruning algorithm
      * @param buttons matrix of buttons representing the board
      * @return best coordinate to move to
      */
     @Override
-    public int[] move(Button[][] buttons) {
-        int c = 0;
-        int max = Integer.MIN_VALUE;
-        int mi = 0,mj = 0;
-        HashSet<Pair<Integer, Integer>> uniquePairs = new HashSet<>();
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                if (buttons[i][j].getText().equals("X") || buttons[i][j].getText().equals("O")) {
-                    int[][] directions = {
-                            {0, 1},
-                            {0, -1},
-                            {1, 0},
-                            {-1, 0},
-                    };
-                    for (int[] dir : directions) {
-                        int newX = i + dir[0];
-                        int newY = j + dir[1];
-                        if (newX >= 0 && newX < 8 && newY >= 0 && newY < 8) {
-                            uniquePairs.add(new Pair<>(newX, newY));
+    public int[] move(Button[][] buttons, int pNumber) {
+        this.playerNumber = pNumber;
+        Callable<int[]> minimaxTask = () -> {
+            int c = 0;
+            int max = Integer.MIN_VALUE;
+            int mi = 0, mj = 0;
+            HashSet<Pair<Integer, Integer>> uniquePairs = new HashSet<>();
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    if (buttons[i][j].getText().equals("X") || buttons[i][j].getText().equals("O")) {
+                        int[][] directions = {
+                                {0, 1},
+                                {0, -1},
+                                {1, 0},
+                                {-1, 0},
+                        };
+                        for (int[] dir : directions) {
+                            int newX = i + dir[0];
+                            int newY = j + dir[1];
+                            if (newX >= 0 && newX < 8 && newY >= 0 && newY < 8 && buttons[newX][newY].getText().equals("")) {
+                                uniquePairs.add(new Pair<>(newX, newY));
+                            }
                         }
                     }
                 }
             }
-        }
-        Pair<Integer, Integer>[] pairs = uniquePairs.toArray(new Pair[0]);
-        for (Pair<Integer, Integer> pair : uniquePairs) {
-            int i = pair.getKey();
-            int j = pair.getValue();
-            if (buttons[i][j].getText().equals("")) {
-                c++;
-                BoardState position = new BoardState(buttons);
-                int minimaxValue = minimax(new BoardState(position, i, j, 'O'), 4, Integer.MIN_VALUE, Integer.MAX_VALUE, true, 0);
-                if (max <= minimaxValue) {
-                    max = minimaxValue;
-                    mi = i;
-                    mj = j;
+            for (Pair<Integer, Integer> pair : uniquePairs) {
+                int i = pair.getKey();
+                int j = pair.getValue();
+                if (buttons[i][j].getText().equals("")) {
+                    c++;
+                    BoardState position = new BoardState(buttons);
+                    int minimaxValue;
+                    if (this.playerNumber == 1){
+                        minimaxValue = minimax(new BoardState(position, i, j, 'X'), 3, Integer.MIN_VALUE, Integer.MAX_VALUE, false, 0);
+                    } else {
+                        minimaxValue = minimax(new BoardState(position, i, j, 'O'), 3, Integer.MIN_VALUE, Integer.MAX_VALUE, false, 0);
+                    }
+                    if (max < minimaxValue) {
+                        max = minimaxValue;
+                        mi = i;
+                        mj = j;
+                    }
                 }
             }
+            int[] ret = {mi, mj};
+            return ret;
+        };
+        Future<int[]> future = executorService.submit(minimaxTask);
+        try {
+            return future.get(TIMEOUT, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            e.printStackTrace();
+            System.out.println("Timeout, get random move!");
+            return getRandomMove(buttons);
+        } finally {
+            future.cancel(true);
         }
-        int[] ret = {mi, mj};
-        return ret;
     }
 
     public int minimax(BoardState position, int depth, int alpha, int beta, boolean maximizingPlayer, int count){
         if (depth == 0 || position.isFull()){
-            return position.evaluateBoard();
+            return position.evaluateBoard(playerNumber);
         }
         if (maximizingPlayer){
             Integer maxEval = Integer.MIN_VALUE;
             for (int i = 0; i < 8; i++) {
                 for (int j = 0; j < 8; j++) {
                     if (position.Positions[i][j] == '_') {
-                        BoardState a = new BoardState(position,i,j,'O');
-                        int eval = minimax(new BoardState(position, i, j, 'O'), depth-1, alpha, beta, false, count+1);
+                        int eval;
+                        if (playerNumber == 1) {
+                            eval = minimax(new BoardState(position, i, j, 'X'), depth-1, alpha, beta, false, count+1);
+                        } else {
+                            eval = minimax(new BoardState(position, i, j, 'O'), depth-1, alpha, beta, false, count+1);
+                        }
                         maxEval = Math.max(eval, alpha);
+                        alpha = Math.max(maxEval,alpha);
                         if (beta <= alpha) {
                             break;
                         }
@@ -76,8 +103,14 @@ public class BotMinimax extends Bot{
             for (int i = 0; i < 8; i++) {
                 for (int j = 0; j < 8; j++) {
                     if (position.Positions[i][j] == '_') {
-                        int eval = minimax(new BoardState(position, i, j, 'X'), depth-1, alpha, beta, true, count+1);
+                        int eval;
+                        if (playerNumber == 1) {
+                            eval = minimax(new BoardState(position, i, j, 'O'), depth-1, alpha, beta, true, count+1);
+                        } else {
+                            eval = minimax(new BoardState(position, i, j, 'X'), depth-1, alpha, beta, true, count+1);
+                        }
                         minEval = Math.min(eval, beta);
+                        beta = Math.max(minEval,beta);
                         if (beta <= alpha) {
                             break;
                         }
@@ -86,5 +119,15 @@ public class BotMinimax extends Bot{
             }
             return minEval;
         }
+    }
+    private int[] getRandomMove(Button[][] buttons) {
+        for (int i = 0 ; i < 8 ; i++){
+            for (int j = 0 ; j < 8 ; j++){
+                if (buttons[i][j].getText().equals("")) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
     }
 }
